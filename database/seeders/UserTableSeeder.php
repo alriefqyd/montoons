266d3bea68477bc3d450d8a1d3a6845962ef9122
Al Riefqy <ekydesmito@gmail.com>
2022-08-13 23:02:22 +0800

    add spatie for user role

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin1@mail.com',
+            'password' => bcrypt('29121996')
+        ]);
+
+        $admin->assignRole('admin'); //a user to role
+    }
+}
